8597c3a9ccd1972d2fedd87e4c7b97e63ff4b6aa
mayda <mayda.werfali@gmail.com>
2021-08-15 20:15:56 +0200

    added sanctum and login and register

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/tokens/create', [Api\UserController::class, 'show']);
+// Route::group(['prefix' => 'api'], function() {
+//     Route::post('/register', "Api\AuthController@register");
+//     Route::post('/login', "Api\AuthController@login");
+// });
+
+Route::post('/register', "Api\AuthController@register");
+Route::post('/login', "Api\AuthController@login");
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
